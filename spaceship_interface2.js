66937a5289401c81238022fe39f1b4acb7e72a25
stephen <stebra2@nb4485.ie.deri.local>
2015-05-26 20:58:25 +0100

    Toggle Button
    Toggle light on and off

--- spaceship_interface2.js
+++ spaceship_interface2.js
@@ -6,6 +6,7 @@ myBoard = new five.Board();
 
 myBoard.on("ready", function() 
 {
+  var lightOn = false;
   var btn = new five.Button(button);
   myLed1 = new five.Led(3);
   myLed2 = new five.Led(4);
@@ -13,21 +14,24 @@ myBoard.on("ready", function()
   
   btn.on("hit", function()
   {
-	  myLed1.on();
+	  if(lightOn == true)
+	  {
+		myLed1.on();
+		lightOn = false;
+	  }
+	  else 
+	  {
+		  myLed1.off();
+		  lightOn = true;
+	  }
+	
   });
   
+  /*
   btn.on("release", function()
   {
 	  myLed1.off();
   });
-  
-  //myLed2.blink(500);
-  //myLed3.strobe(500);
-  //myLed1.strobe( 3000 );
-	
-  // make myLED available as "led" in REPL
+  */
 
-  // this.repl.inject({
-    // led: myLed
-  // });
 });
