f2f7e9bd703f4c2c4cc578eb5a6a5b7567899fa6
stephen <stebra2@nb4485.ie.deri.local>
2015-06-02 20:31:27 +0100

    NodeJS light on light off

---
+++ RGB_light.js
@@ -0,0 +1,19 @@
+var five = require("johnny-five");
+var myBoard, myLed;
+var button = 2;
+
+myBoard = new five.Board();
+
+myBoard.on("ready", function() 
+{
+  var lightOn = false;
+  var btn = new five.Button(button);
+  myLed1 = new five.Led(11);
+  myLed2 = new five.Led(10);
+  myLed3 = new five.Led(9);
+  
+ myLed2.on();
+ myLed1.on();
+
+
+});
